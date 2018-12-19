1ba08da67e02ce486e379c64814e5bc7b208b714
lukazko <lukaz.kubik@gmail.com>
2018-12-19 20:49:08 +0100

    Obhájená verze

--- js/quiz.js
+++ js/quiz.js
@@ -1,202 +1,124 @@
-const quizContainer = document.querySelector('#quiz');
-const resultsContainer = document.querySelector('#results');
-const newButton = document.querySelector('#new');
-const submitButton = document.querySelector('#submit');
-const shareButton = document.querySelector('#share');
-var alertWindow = document.querySelector('.alert-window');
-var closeWindow = document.querySelectorAll('.close')[0];
-var message = document.querySelector('.alert-window-text');
+const quizContainer = document.querySelector("#quiz");
+const resultsContainer = document.querySelector("#results");
+const newButton = document.querySelector("#new");
+const submitButton = document.querySelector("#submit");
+const shareButton = document.querySelector("#share");
+var alertWindow = document.querySelector(".alert-window");
+var closeWindow = document.querySelectorAll(".close")[0];
+var message = document.querySelector(".alert-window-text");
+var listOfQuestions = [];
 
-const listOfQuestions = [
-    {
-        question: "Ahoj, vítej v mém kvízu, nebudeme otálet a jdeme rovnou na věc!",
-        answers: {
-            a: "Ahoj!",
-            b: "Tvoje máma je kvíz!",
-            c: "Život je life",
-            d: "4IZ268"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Jaký je tvůj nejoblíbenější kousek z merche The Boring Company?",
-        answers: {
-            a: "Flamethrower",
-            b: "Kšiltovka",
-            c: "not a Flamethrower"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Petr nikdy nemluví pravdu. Zuzana vždycky lže. Zuzana tvrdí, že Petr na sobě má zelený svetr. Petr říká, že tomu tak není. Kolik štěňat má Standa?",
-        answers: {
-            a: "Nevím :(",
-            b: "3",
-            c: "Standa má surikatu",
-            d: "Panu Jiřímu smrdí nohy"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "Který z Finwëho synů je tvůj nejoblíbenější?",
-        answers: {
-            a: "Fëanor",
-            b: "Fingolfin",
-            c: "Finarfin",
-            d: "Galadriel"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "Myslíš si, že je Schrödingerova kočka naživu?",
-        answers: {
-            a: "Ano",
-            b: "Ne",
-            c: "Kvantová mechanika nefunguje v makrosvětě :(",
-            d: "Penny je blbá blondýna"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Jaké je jméno boha?",
-        answers: {
-            a: "Alláh",
-            b: "Nietzsche",
-            c: "Škromach",
-            d: "Jehova"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "Který z Avengerů je nejmocnější?",
-        answers: {
-            a: "Thor",
-            b: "Hawkeye",
-            c: "Božan Drtitel",
-            d: "Ta holka z HIMYM"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "Kde se nachází takzvaný ráj surferov?",
-        answers: {
-            a: "Austrálie",
-            b: "Na měsíci Europa",
-            c: "Na Bali",
-            d: "Oravská priehrada"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "S kterou další postavou z Ulice přišel Matěj Jordán o panictví?",
-        answers: {
-            a: "S vnučkou pana Peška",
-            b: "S paní Niklovou z večerky",
-            c: "S Bedřichem Liškou, nebezpečným trestancem",
-            d: "Se ségrou Terezou"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "Tak jsme na konci. Čeká tě už jen poslední zapeklitá otázka. Líbil se ti tento kvíz?",
-        answers: {
-            a: "13",
-            b: "21",
-            c: "42",
-            d: "759"
-        },
-        correctAnswer: "c"
-    }
-];
+var buildQuiz = function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.myjson.com/bins/10e4gw");
+  xhr.addEventListener("load", function() {
+    var data = JSON.parse(xhr.responseText);
 
-var buildQuiz = function () {
-
+    listOfQuestions = data;
     resultsContainer.innerHTML = "";
     var output = [];
+    quizContainer.innerHTML = '<img src="img/loader.svg">';
 
     listOfQuestions.forEach((currentQuestion, questionNumber) => {
-        var answers = [];
+      var answers = [];
 
-        for (letter in currentQuestion.answers) {
-            answers.push(
-                `<label>
-              <input type="radio" id="question${questionNumber}" name="question${questionNumber}" class="disableMe" value="${letter}">
-              ${letter} :
+      for (letter in currentQuestion.answers) {
+        answers.push(
+          `<label>
+              <input type="checkbox" name="question${questionNumber}" class="disableMe" value="${letter}">
               ${currentQuestion.answers[letter]}
             </label>`
-            );
-        }
-
-        output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join("")} </div>`
         );
+      }
+
+      answers.sort(function() {
+        return 0.5 - Math.random();
+      });
+
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join("")} </div>`
+      );
     });
 
     quizContainer.innerHTML = output.join("");
-}
+  });
+  xhr.send();
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+};
 
-var getResults = function () {
-    if (resultsContainer.innerHTML) {
-        return false;
-    }
+var getResults = function() {
+  if (resultsContainer.innerHTML) {
+    return false;
+  }
 
-    var answerContainers = quizContainer.querySelectorAll(".answers");
-    var nCorrect = 0;
+  var answerContainers = quizContainer.querySelectorAll(".answers");
+  nCorrect = 0;
 
-    listOfQuestions.forEach((currentQuestion, questionNumber) => {
-        var answerContainer = answerContainers[questionNumber];
-        var selector = `input[id=question${questionNumber}]:checked`;
-        var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-        if (userAnswer === currentQuestion.correctAnswer) {
-            nCorrect++;
-            answerContainers[questionNumber].style.color = "lightgreen";
-        }
-        else {
-            answerContainers[questionNumber].style.color = "red";
-        }
+  listOfQuestions.forEach((currentQuestion, questionNumber) => {
+    var answerContainer = answerContainers[questionNumber];
+    var zk = [];
+    $(`input[name=question${questionNumber}]:checked`).each(function() {
+      zk.push($(this).val());
     });
 
-    if (nCorrect >= 5) {
-        message.innerHTML = '<h2>Dobrá práce!</h2><br> Tvůj výsledek je: <strong>' + nCorrect + ' správných odpovědí z ' + listOfQuestions.length + '</strong>';
-        alertWindow.style.display = "block";
+    zk.sort();
+    if (zk.toString() === currentQuestion.correctAnswer.toString()) {
+      nCorrect++;
+      answerContainer.style.color = "lightgreen";
+    } else {
+      answerContainer.style.color = "red";
     }
-    else {
-        message.innerHTML = '<h2>Nic moc kámo...</h2><br> Tvůj výsledek je: <strong>' + nCorrect + ' správných odpovědí z ' + listOfQuestions.length + '</strong>';
-        alertWindow.style.display = "block";
-    }
+  });
 
-    resultsContainer.innerHTML = 'Tvůj výsledek je: <strong>' + nCorrect + ' správných odpovědí z ' + listOfQuestions.length + '</strong>';
-    disableInputs();
+  if (nCorrect >= 5) {
+    message.innerHTML =
+      "<h2>Dobrá práce!</h2><br> Tvůj výsledek je: <strong>" +
+      nCorrect +
+      " správných odpovědí z " +
+      listOfQuestions.length +
+      "</strong>";
+    alertWindow.style.display = "block";
+  } else {
+    message.innerHTML =
+      "<h2>Nic moc kámo...</h2><br> Tvůj výsledek je: <strong>" +
+      nCorrect +
+      " správných odpovědí z " +
+      listOfQuestions.length +
+      "</strong>";
+    alertWindow.style.display = "block";
+  }
+
+  resultsContainer.innerHTML =
+    "Tvůj výsledek je: <strong>" +
+    nCorrect +
+    " správných odpovědí z " +
+    listOfQuestions.length +
+    "</strong>";
+  disableInputs();
 };
 
-var disableInputs = function () {
-    var radios = document.querySelectorAll('.disableMe');
-
-    for (var i = 0; i < radios.length; i++) {
-        radios[i].disabled = true;
-    }
+var disableInputs = function() {
+  $(".disableMe").prop("disabled", true);
 };
 
-var share = function () {
-    if (!resultsContainer.innerHTML) {
-        return false;
-    }
+var share = function() {
+  if (!resultsContainer.innerHTML) {
+    return false;
+  }
 
-    var url = "https://twitter.com/intent/tweet";
-    var text = $('#results').text();
-    var via = "lukazko";
-    var hashtag = "lukazko_quiz";
-    url = url + "?text=" + text + ";hashtags=" + hashtag + ";via=" + via;
+  var url = "https://twitter.com/intent/tweet";
+  var text = $("#results").text();
+  var via = "lukazko";
+  url = url + "?text=" + text + ";via=" + via;
 
-    window.open(url, "Sdílet výsledek na Twitter", "width=650,height=250");
-}
+  window.open(url, "Sdílet výsledek na Twitter", "width=650,height=250");
+};
 
-closeWindow.onclick = function () {
-    alertWindow.style.display = "none";
-    message.innerHTML = "";
-}
+closeWindow.onclick = function() {
+  alertWindow.style.display = "none";
+  message.innerHTML = "";
+};
 
 buildQuiz();
 submitButton.addEventListener("click", getResults);
--- js/weather.js
+++ js/weather.js
@@ -1,97 +1,109 @@
-const refreshButton = document.querySelector('#refresh');
-$("#summary").text("Povol přístup k poloze!");
+const refreshButton = document.querySelector("#refresh");
+var weatherIcon = document.querySelector("#weather-icon");
+var summaryText = document.querySelector("#summary");
+var tempText = document.querySelector("#temp");
+var windText = document.querySelector("#wind");
 
-var getPosition = function () {
-    var positions = {}
-    $("#weather-icon").attr("src", "img/loader.svg");
-    return new Promise((resolve) => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                positions.lat = position.coords.latitude;
-                positions.lon = position.coords.longitude;
-                resolve(positions);
-                console.log(positions);
-            });
-        }
-    });
-}
+var setLoader = function() {
+  weatherIcon.src = "img/loader.svg";
+  summaryText.innerHTML = "Povol přístup k poloze!";
+  tempText.innerHTML = "";
+  windText.innerHTML = "";
+};
+
+var getPosition = function() {
+  var positions = {};
+  setLoader();
+  return new Promise(resolve => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        positions.lat = position.coords.latitude;
+        positions.lon = position.coords.longitude;
+        resolve(positions);
+        //console.log(positions);
+      });
+    }
+  });
+};
 
 function tempConvert(temp) {
-    var temp = Math.round((temp - 32) * 5 / 9);
-    return temp;
+  var temp = Math.round(((temp - 32) * 5) / 9);
+  return temp;
 }
 
 function setIcon(icon) {
-    switch (icon) {
-        case 'clear-day':
-            $("#weather-icon").attr("src", "img/weather/clear-day.png");
-            break;
-        case 'clear-night':
-            $("#weather-icon").attr("src", "img/weather/clear-night.png");
-            break;
-        case 'rain':
-            $("#weather-icon").attr("src", "img/weather/rain.png");
-            break;
-        case 'snow':
-            $("#weather-icon").attr("src", "img/weather/snow.png");
-            break;
-        case 'cloudy':
-            $("#weather-icon").attr("src", "img/weather/cloudy.png");
-            break;
-        case 'thunderstorm':
-            $("#weather-icon").attr("src", "img/weather/cloudy.png");
-            break;
-        case 'sleed':
-            $("#weather-icon").attr("src", "img/weather/cloudy.png");
-            break;
-        case 'wind':
-            $("#weather-icon").attr("src", "img/weather/wind.png");
-            break;
-        case 'fog':
-            $("#weather-icon").attr("src", "img/weather/fog.png");
-            break;
-        case 'partly-cloudy-day':
-            $("#weather-icon").attr("src", "img/weather/partly-cloudy-day.png");
-            break;
-        case 'partly-cloudy-night':
-            $("#weather-icon").attr("src", "img/weather/partly-cloudy-night.png");
-            break;
-    }
+  switch (icon) {
+    case "clear-day":
+      weatherIcon.src = "img/weather/clear-day.png";
+      break;
+    case "clear-night":
+      weatherIcon.src = "img/weather/clear-night.png";
+      break;
+    case "rain":
+      weatherIcon.src = "img/weather/rain.png";
+      break;
+    case "snow":
+      weatherIcon.src = "img/weather/snow.png";
+      break;
+    case "cloudy":
+      weatherIcon.src = "img/weather/cloudy.png";
+      break;
+    case "thunderstorm":
+      weatherIcon.src = "img/weather/cloudy.png";
+      break;
+    case "sleed":
+      weatherIcon.src = "img/weather/cloudy.png";
+      break;
+    case "wind":
+      weatherIcon.src = "img/weather/wind.png";
+      break;
+    case "fog":
+      weatherIcon.src = "img/weather/fog.png";
+      break;
+    case "partly-cloudy-day":
+      weatherIcon.src = "img/weather/partly-cloudy-day.png";
+      break;
+    case "partly-cloudy-night":
+      weatherIcon.src = "img/weather/partly-cloudy-night.png";
+      break;
+  }
 }
 
 function getWeather(position) {
-    var url = "https://api.darksky.net/forecast/0d66f2f1604ceeffb4daf23447d6a265/" + position.lat + "," + position.lon + "?lang=cs";
-    var weather = $.ajax({
-        url: url,
-        dataType: "jsonp"
-    });
+  var url =
+    "https://api.darksky.net/forecast/0d66f2f1604ceeffb4daf23447d6a265/" +
+    position.lat +
+    "," +
+    position.lon +
+    "?lang=cs";
+  var weather = $.ajax({
+    url: url,
+    dataType: "jsonp"
+  });
 
-    weather.done(function (res) {
-        console.log(res)
-        var currently = res.currently;
-        //var daily = res.daily;
-        var summary = currently.summary;
-        var temp = 'Teplota je ' + tempConvert(currently.temperature) + ' °C';
-        var wind = 'Vítr o rychlosti ' + currently.windSpeed + ' m/s';
-        setIcon(currently.icon);
-        //$("#weather-icon").attr("src", "img/weather/wind.png");
-        $("#summary").text(summary);
-        $("#temp").text(temp);
-        $("#wind").text(wind);
-    });
+  weather.done(function(res) {
+    //console.log(res)
+    var currently = res.currently;
+    var summary = currently.summary;
+    var temp = "Teplota je " + tempConvert(currently.temperature) + " °C";
+    var wind = "Vítr o rychlosti " + currently.windSpeed + " m/s";
+    setIcon(currently.icon);
+    summaryText.innerHTML = summary;
+    tempText.innerHTML = temp;
+    windText.innerHTML = wind;
+  });
 }
 
-$(document).ready(function () {
-    getPosition().then(position => {
-        getWeather(position);
-    });
+$(document).ready(function() {
+  setLoader();
+  getPosition().then(position => {
+    getWeather(position);
+  });
 });
 
-refreshButton.addEventListener('click', function () {
-    $("#summary").text("Povol přístup k poloze!");
-    $("#temp").text("");
-    $("#wind").text("");
-    getPosition().then(position => {
-        getWeather(position);
-    });
+refreshButton.addEventListener("click", function() {
+  setLoader();
+  getPosition().then(position => {
+    getWeather(position);
+  });
 });
